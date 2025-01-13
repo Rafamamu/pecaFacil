7a95a6bb3725d3a372f44c502a1c57cd150d2dd8
Rafael <rafaroga46@gmail.com>
2025-01-13 18:39:45 -0300

    first commit

---
+++ src/main/java/com/rafamamu/pecaFacil/PecaFacilApplication.java
@@ -0,0 +1,13 @@
+package com.rafamamu.pecaFacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecaFacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafamamu/pecaFacil/PecaFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafamamu.pecaFacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecaFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
