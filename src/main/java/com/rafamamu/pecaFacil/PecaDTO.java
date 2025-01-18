baaba10412247c40809dec872e80e1dfccd17197
Rafael <rafaroga46@gmail.com>
2025-01-18 20:28:29 -0300

    refact: Adiciona as Annotations lombok

--- src/main/java/com/rafamamu/pecaFacil/PecaDTO.java
+++ src/main/java/com/rafamamu/pecaFacil/PecaDTO.java
@@ -1,6 +1,12 @@
 package com.rafamamu.pecaFacil;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PecaDTO {
     private Long id;
     private String codigo;
